d4ff9715afe6512d6500a139a8c3c6cd72def417
khansatanveer <160322911+khansatanveer@users.noreply.github.com>
2024-12-17 20:11:05 +0500

    add files

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     <header className="flex h-[100px] items-center justify-center">
       <div className="grid grid-cols-2 sm:grid-cols-1 lg:grid-cols-1">
         {/* Navigation */}
-        <div className="absolute top-[20px] left-4 lg:top-[38px] lg:left-[505px] sm:top-[38px] sm:left-[130px] md:left-[220px]">
+        <div className="absolute top-[20px] left-4 lg:top-[38px]  sm:top-[38px] sm:left-[130px] md:left-[220px] lg:left-[400px]">
           <nav className="flex gap-4 sm:gap-4 md:gap-5 lg:gap-20 text-[15px] sm:text-[16px] lg:text-[18px] px-6 lg:px-0 font-medium">
             <Link href="/Home" className="text-black hover:underline leading-7">
               Home
@@ -28,7 +28,7 @@ const Header = () => {
         </div>
 
         {/* Icons */}
-        <div className="flex gap-5 sm:gap-3 md:gap-4 lg:gap-[40px] ml-0 md:mr-[-90px] sm:ml-[440px] lg:ml-[1093px] px-24 sm:px-6 lg:px-0 mr-4 lg:mr-12 mt-8 sm:mt-0 lg:mt-0 ">
+        <div className="flex gap-5 sm:gap-3 md:gap-4 lg:gap-[40px] ml-0 md:mr-[-90px] sm:ml-[440px] lg:ml-[980px] px-24 sm:px-6 lg:px-0 mr-4 lg:mr-12 mt-8 sm:mt-0 lg:mt-0 ">
 
           <a href="../Account" rel="noopener noreferrer" aria-label="Profile">
             <Image
