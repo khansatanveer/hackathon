cf341fc0f0c3df4abe1bad578bb2866d229ed2f0
khansatanveer <160322911+khansatanveer@users.noreply.github.com>
2024-12-18 21:19:12 +0500

    add files

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -12,8 +12,9 @@ import Pic7 from '@/app/public/Rectangle 14.png'
 import Pic8 from '@/app/public/Rectangle 15.png'
 import Pic9 from '@/app/public/bg-2.png'
 import Clock from "@/app/public/clock-fill.png";
-import  Calendar from "@/app/public/Calendar .png";
+import Calendar from "@/app/public/Calendar .png";
 import Header from '@/app/components/header'
+
 
 const Home = () => {
   return (
@@ -21,26 +22,26 @@ const Home = () => {
       <div className="bg-[#FBEBB5] h-[140px]">
         <Header />
       </div>
-      <div className="w-full h-screen bg-[#FBEBB5] flex flex-col lg:flex-row-reverse items-center justify-center ">
+      <div className="w-full h-screen bg-[#FBEBB5] flex flex-col lg:flex-row-reverse items-center justify-center relative overflow-hidden">
         {/* Right Content: Image */}
         <div className="mt-[-150px] sm:mt-[-90px] lg:mt-0 mb-2 lg:mr-[-130px] lg:mb-[100px]">
           <Image
             src={Sofa}
             alt="Sofa Single Seater"
-            className="md:w-[600px] md:h-[700px] w-[500px] h-[400px] lg:h-[600px] lg:w-[500px] scale-x-[-1]"
+            className="md:w-[600px] md:h-[700px] w-[500px] h-[400px] lg:h-[600px] lg:w-[600px] scale-x-[-1]"
             priority
           />
         </div>
 
         {/* Left Content: Text */}
         <div className="text-center lg:text-left lg:ml-[-40px]">
-          <h1 className="text-[#000000] font-medium text-[30px] sm:text-[48px] lg:text-[58px] leading-[40px] sm:leading-[60px] lg:leading-[96px] lg:mt-[0px] mt-[-80px]">
-            Rocket single <br /> seater
+          <h1 className="text-[#000000] font-medium text-[30px] sm:text-[48px] lg:text-[54px] leading-[40px] sm:leading-[60px] lg:leading-[96px] lg:mt-[0px] mt-[-80px]">
+            Rocket single <br />seater
           </h1>
           <br />
-          <button className="font-medium hover:text-[23px] hover:sm:text-[23px] text-[18px] sm:text-[22px] lg:text-[20px] text-black lg:mt-[0px] mt-[-70px]">
+          <button className="font-medium hover:text-[23px] hover:sm:text-[23px] text-[18px] sm:text-[22px] lg:text-[22px] text-black lg:mt-[-12px] mt-[-70px]">
             Shop Now
-            <hr className="mt-1 border-1  border-black lg:w-[121px] w-[90px] mx-auto mb-3" />
+            <hr className="mt-1 border  border-black lg:w-[121px] w-[90px] mx-auto mb-5" />
           </button>
         </div>
       </div>
@@ -58,7 +59,7 @@ const Home = () => {
             <a href="../Shop" rel="noopener noreferrer" aria-label="Profile">
               <button className="mt-[-24px] lg:mt-[-8px] text-[20px] sm:text-[26px] lg:text-[28px] font-medium leading-[36px] hover:text-[24px] hover:sm:text-[30px] hover:lg:text-[32px]">
                 View More
-                <hr className="mt-1  border-1 border-black lg:w-[121px] w-[90px] mx-auto" />
+                <hr className="mt-1  border border-black lg:w-[121px] w-[90px] mx-auto" />
               </button>
             </a>
           </div>
@@ -75,7 +76,7 @@ const Home = () => {
             <a href="../Shop" rel="noopener noreferrer" aria-label="Profile">
               <button className="mt-[-24px] lg:mt-[-8px] text-[20px] sm:text-[26px] lg:text-[28px] font-medium leading-[36px] hover:text-[24px] hover:sm:text-[30px] hover:lg:text-[32px]">
                 View More
-                <hr className="mt-1  border-1 border-black lg:w-[121px] w-[90px] mx-auto" />
+                <hr className="mt-1  border border-black lg:w-[121px] w-[90px] mx-auto" />
               </button>
             </a>
           </div>
@@ -141,7 +142,7 @@ const Home = () => {
           <a href="../Shop" rel="noopener noreferrer" aria-label="Profile">
             <button className="h-[50px] text-[24px] sm:text-[26px] lg:text-[28px] text-black font-medium leading-[36px] hover:text-[24px] hover:sm:text-[30px] hover:lg:text-[32px] mt-10 mb-10">
               View More
-              <hr className="mt-2 lg:mt-3 border-1  border-black w-[120px] mx-auto" />
+              <hr className="mt-2 lg:mt-3 border  border-black w-[120px] mx-auto" />
             </button>
           </a>
         </div>
@@ -170,19 +171,19 @@ const Home = () => {
           <p className="text-gray-600 mb-8 text-lg sm:text-xl lg:text-2xl">Find a bright idea to suit your taste with our great selection</p>
           <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-8 md:px-4 ">
             {/* Blog Card 1 */}
-            <div className="bg-white h-[400px] sm:h-[450px] lg:h-[580px]  w-[300px] sm:w-[350px] lg:w-full shadow-lg rounded-lg overflow-hidden mx-auto">
+            <div className="bg-white h-[400px] sm:h-[450px] lg:h-[650px]  w-[300px] sm:w-[350px] lg:w-full shadow-lg rounded-lg overflow-hidden mx-auto">
               <Image
                 src={Pic6}
                 alt="Blog 1"
                 className="w-full h-[200px] sm:h-[250px] lg:h-[393px] object-cover"
               />
               <div className="p-6 sm:p-7 lg:p-8">
-                <h3 className="text-[18px] sm:text-[18px] lg:text-[20px] font-medium mb-5">
+                <h3 className="text-[18px] sm:text-[18px] lg:text-[18px] font-medium mb-5">
                   Going all-in with millennial design
                 </h3>
                 <a href="#" className="text-black  font-medium text-base sm:text-lg lg:text-xl hover:lg:text-[24px] hover:sm:text-[20px] hover:text-[18px]">
                   Read More
-                  <hr className="mt-1 lg:mt-3  border-1 border-black w-[100px] sm:w-[120px] mx-auto" />
+                  <hr className="mt-1 lg:mt-3  border border-black w-[100px] sm:w-[120px] mx-auto" />
                 </a>
                 <div className="flex justify-center gap-4 mt-3 lg:mt-7">
                   <span className="flex items-center gap-1 text-[#9F9F9F] text-sm">
@@ -201,19 +202,19 @@ const Home = () => {
               </div>
             </div>
             {/* Blog Card 2 */}
-            <div className="bg-white h-[400px] sm:h-[450px] lg:h-[580px] w-[300px] sm:w-[350px] lg:w-full shadow-lg rounded-lg overflow-hidden mx-auto">
+            <div className="bg-white h-[400px] sm:h-[450px] lg:h-[650px] w-[300px] sm:w-[350px] lg:w-full shadow-lg rounded-lg overflow-hidden mx-auto">
               <Image
                 src={Pic7}
                 alt="Blog 2"
                 className="w-full h-[200px] sm:h-[250px] lg:h-[393px] object-cover"
               />
               <div className="p-6 sm:p-7 lg:p-8">
-                <h3 className="text-[18px] sm:text-[18px] lg:text-[20px] font-medium mb-5">
+                <h3 className="text-[18px] sm:text-[18px] lg:text-[18px] font-medium mb-5">
                   Going all-in with millennial design
                 </h3>
                 <a href="../Blog" rel="noopener noreferrer" aria-label="Profile" className="text-black  font-medium text-base sm:text-lg lg:text-xl hover:lg:text-[24px] hover:sm:text-[20px] hover:text-[18px]">
                   Read More
-                  <hr className="mt-1 lg:mt-3  border-1 border-black w-[100px] sm:w-[120px] mx-auto" />
+                  <hr className="mt-1 lg:mt-3  border border-black w-[100px] sm:w-[120px] mx-auto" />
                 </a>
                 <div className="flex justify-center gap-4 mt-3 lg:mt-7">
                   <span className="flex items-center gap-1 text-[#9F9F9F] text-sm">
@@ -233,19 +234,19 @@ const Home = () => {
               </div>
             </div>
             {/* Blog Card 3 */}
-            <div className="bg-white h-[400px] sm:h-[450px] lg:h-[580px] w-[300px] sm:w-[350px] lg:w-full shadow-lg rounded-lg overflow-hidden mx-auto">
+            <div className="bg-white h-[400px] sm:h-[450px] lg:h-[650px] w-[300px] sm:w-[350px] lg:w-full shadow-lg rounded-lg overflow-hidden mx-auto">
               <Image
                 src={Pic8}
                 alt="Blog 3"
                 className="w-full h-[200px] sm:h-[250px] lg:h-[393px] object-cover"
               />
               <div className="p-6 sm:p-7 lg:p-8">
-                <h3 className="text-[18px] sm:text-[18px] lg:text-[20px] font-medium mb-5">
+                <h3 className="text-[18px] sm:text-[18px] lg:text-[18px] font-medium mb-5">
                   Going all-in with millennial design
                 </h3>
                 <a href="#" className="text-black  font-medium text-base sm:text-lg lg:text-xl hover:lg:text-[24px] hover:sm:text-[20px] hover:text-[18px]">
                   Read More
-                  <hr className="mt-1 lg:mt-3  border-1 border-black w-[100px] sm:w-[120px] mx-auto" />
+                  <hr className="mt-1 lg:mt-3  border border-black w-[100px] sm:w-[120px] mx-auto" />
                 </a>
                 <div className="flex justify-center gap-4 mt-3 lg:mt-7">
                   <span className="flex items-center gap-1 text-[#9F9F9F] text-sm">
@@ -266,7 +267,7 @@ const Home = () => {
             <a href="../Blog" rel="noopener noreferrer" aria-label="Profile">
               <button className="lg:mr-[-900px] h-[50px] text-[24px] sm:text-[26px] lg:text-[28px] text-black font-medium leading-[36px] hover:text-[24px] hover:sm:text-[30px]  hover:lg:text-[32px] mt-5 mb-5">
                 View More
-                <hr className="mt-2 lg:mt-3  border-1  border-black w-[120px] mx-auto" />
+                <hr className="mt-2 lg:mt-3  border  border-black w-[120px] mx-auto" />
               </button>
             </a>
           </div>
