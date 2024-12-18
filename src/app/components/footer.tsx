4cdd5c23d891b325045cc3b4b353f86e27fa189a
khansatanveer <160322911+khansatanveer@users.noreply.github.com>
2024-12-18 19:58:02 +0500

    add files

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,15 +3,17 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="h-[400px] sm:h-[300px] lg:h-[555px] py-16 sm:py-16 lg:py-24 bg-white">
-      <div className="max-w-screen-sm sm:max-w-screen-sm lg:max-w-screen-xl h-auto sm:h-auto lg:h-[312px] w-auto mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-10 lg:gap-20">
+    <footer className="min-h-screen overflow-hidden  mt-8 sm:mt-8 md:mt-8 lg:mt-24 bg-white">
+      <div className="max-w-screen-sm sm:max-w-screen-sm lg:max-w-screen-xl h-auto sm:h-auto lg:h-[312px] w-auto mx-auto  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-10 lg:gap-20 lg:px-12">
+        
         {/* First Column: Address */}
-        <div className=" mb-8 sm:mt-9 lg:mt-40 text-[14px] sm:text-[15px] lg:text-[16px] px-5 sm:px-5 lg:px-0 text-[#9F9F9F]">
-          <p>400 University Drive Suite 200 Coral<br /> Gables,<br /> FL 33134 USA</p>
+        <div className="mb-8 sm:mt-9 lg:mt-40 text-[14px] sm:text-[15px] lg:text-[16px] px-5 sm:px-5 lg:px-0 text-[#9F9F9F]">
+          <p>400 University Drive Suite 200 Coral<br />Gables,<br />FL 33134 USA</p>
         </div>
 
         {/* Links Section */}
         <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 px-5 sm:px-0 lg:px-0 gap-10 sm:gap-5 lg:gap-64">
+          
           {/* Links */}
           <div className="flex flex-col sm:ml-0 lg:ml-16 gap-6 sm:gap-8 lg:gap-16 mb-9 lg:mb-0">
             <h6 className="text-[#9F9F9F] text-[15px] sm:text-[16px] lg:text-[16px] font-medium">Links</h6>
@@ -20,7 +22,8 @@ const Footer = () => {
             <Link href="/About" className="text-[13px] sm:text-[14px] lg:text-[16px] hover:text-blue-800">About</Link>
             <Link href="/Contact" className="text-[13px] sm:text-[14px] lg:text-[16px] hover:text-blue-800">Contact</Link>
           </div>
-          {/* Help */}
+          
+          {/* Help Section */}
           <div className="flex flex-col sm:gap-8 lg:gap-16 gap-6">
             <h6 className="font-medium text-[15px] sm:text-[16px] lg:text-[16px] text-[#9F9F9F]">Help</h6>
             <Link href="/payment-options" className="font-medium text-[13px] sm:text-[14px] lg:text-[16px] hover:text-blue-800">Payment Options</Link>
@@ -37,22 +40,22 @@ const Footer = () => {
               <input
                 type="text"
                 placeholder="Enter Your Email Address"
-                className="w-[180px] sm:w-[300px] lg:w-[230px] text-center sm:text-left text-[12px] sm:text-[14px] lg:text-[16px] h-[18px] sm:h-[20px] lg:h-[24px]  focus:outline-none"
+                className="w-[180px] sm:w-[300px] lg:w-[230px] text-center sm:text-left text-[12px] sm:text-[14px] lg:text-[16px] h-[18px] sm:h-[20px] lg:h-[24px] focus:outline-none"
               />
-              <hr className="mt-1 sm:border-1 lg:border-2 border-black w-[200px] sm:w-[350px] " />
+              <hr className="mt-1 border border-black w-[200px] sm:w-[350px]" />
             </div>
             <div className="px-16 sm:px-5 lg:px-0">
               <button className="text-black hover:text-red-600 font-medium text-[13px] sm:text-[14px] lg:text-[16px]">SUBSCRIBE</button>
-              <hr className="mt-1 sm:border-1 lg:border-2 border-black w-[80px] sm:w-[90px] " />
+              <hr className="mt-1   border border-black w-[80px] sm:w-[90px]" />
             </div>
           </div>
         </div>
       </div>
-
-      <div className="mt-5 sm:mt-12 lg:mt-16 text-center">
-        <hr className="mt-8 sm:mt-12 lg:mt-24 sm:border-1 lg:border-1 border-[#9F9F9F] w-[300px] sm:w-[570px] lg:w-[1300px] mx-auto" />
-        {/* Footer Bottom Section */}
-        <div className="py-4 sm:py-6 lg:py-8 px-10 sm:px-8 lg:px-20 text-left text-[13px] sm:text-[15px] lg:text-[16px]">
+{/* Footer Bottom Section */}
+      <div className=" text-center">
+        <hr className="mt-8 sm:mt-12 lg:mt-44 border border-[#9F9F9F] w-[300px] sm:w-[570px] lg:w-[1100px] mx-auto" />
+        
+        <div className="mb-5 sm:mb-10  mt-4 sm:mt-6 lg:mt-8 px-10 sm:px-8 md:px-24 lg:px-20 text-left text-[13px] sm:text-[15px] lg:text-[16px]">
           <p>2022 Meubel House. All rights reserved.</p>
         </div>
       </div>
